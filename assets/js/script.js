29cca1e50c7b567ba6d078d0b417b768ad175450
EllisJLC <jia.liang.chen@ryerson.ca>
2022-09-28 00:38:16 -0400

    added search and save function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,107 @@
 let api_key = "c04ed60909d66b558fcb4c66996ce63a"; // Save API key
 let url; // declare url variable
 let five_day = document.getElementById("weather-cards");
+let search = document.getElementById("search");
+let cityName = document.getElementById("city");
+let saveCity = document.getElementById("save");
+let navigate = document.querySelector(".navbar");
 let week = ["Sun","Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]; // Days of the week array
-let city = "toronto";
-document.getElementById("date").innerHTML = moment().format("dddd, MMMM Do, YYYY")
+let cityDefault = "Toronto";
+let citySearch;
+let searchedCity;
+document.getElementById("date").innerHTML = moment().format("dddd, MMMM Do, YYYY");
 
-fill_main(city);
+
+load_tabs();
+fill_main(cityDefault);
+
+search.addEventListener("click", (e) => {
+  e.preventDefault();
+  citySearch = document.getElementById("search-text");
+  searchedCity = citySearch.value;
+  citySearch.value = "";
+  fill_main(searchedCity);
+})
+
+saveCity.addEventListener("click", (e) => {
+    if (searchedCity){
+      e.preventDefault();
+      city = searchedCity;
+      if (localStorage.getItem("saved-2")) {
+        localStorage.setItem("saved-2",localStorage.getItem("saved-1"));
+        localStorage.setItem("saved-1",localStorage.getItem("saved-0"));
+        localStorage.setItem("saved-0",city);
+      } else if (localStorage.getItem("saved-1")) {
+        localStorage.setItem("saved-2",city);
+      } else if (localStorage.getItem("saved-0")) {
+        localStorage.setItem("saved-1",city);
+      } else {
+        localStorage.setItem("saved-0",city);
+      }
+      load_tabs();
+    } else {
+    alert ("Please search a city first!");
+  }
+}
+)
+
+navigate.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.tagName === "A"){
+    let citySelect = e.target.textContent;
+    fill_main(citySelect);
+  }
+});
+
+function load_tabs() {
+  let navtabs = document.querySelector(".nav-item");
+  if (localStorage.getItem("saved-0")) {
+    navtabs.children[1].innerHTML = localStorage.getItem("saved-0");
+  } 
+  if (localStorage.getItem("saved-1")) {
+    navtabs.children[2].innerHTML = localStorage.getItem("saved-1");
+  } 
+  if (localStorage.getItem("saved-2")) {
+    navtabs.children[3].innerHTML = localStorage.getItem("saved-2");
+  }
+}
 
 function fill_main (city) {
-    fetch (`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${api_key}`) // Default load to Toronto
+  fetch (`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${api_key}`) // Search load
     .then((response) => response.json())
     .then((data) => {
-      console.log(data)
       if (data.length>=1) {
-      let lat = data[0].lat; // Obtain latitude of Toronto
-      let lon = data[0].lon; // Obtain longitude of Toronto
-      url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}`;
-      fetch (url)
-        .then ((response) => response.json())
-        .then ((data) => {
-          console.log(data);
-          let currentWeather = document.getElementById("day-weather");
-          let skies = data.list[0].weather[0].main; // gets main weather type
-          let weatherIcon = weather_icon(skies);
-          currentWeather.children[3].children[0].textContent = weatherIcon;
-          currentWeather.children[3].children[1].textContent = `Currently ${Math.round(data.list[0].main.temp)-273}°C`; // Display current temperature
-          currentWeather.children[3].children[2].textContent = `Feels like ${Math.round(data.list[0].main.feels_like)-273}°C`; // Display feels like temperature
-          currentWeather.children[3].children[3].textContent = `Humidity: ${data.list[0].main.humidity}%`; // Set humidity
-          let direction = set_direction(data.list[0].wind.deg); // Get wind direction
-          currentWeather.children[3].children[4].textContent = `Wind Speeds: ${data.list[0].wind.speed}km/h ${direction}`; // Set wind speed and direction
-        });
+        cityName.innerHTML = city;
+        let lat = data[0].lat; // Obtain latitude of specified city
+        let lon = data[0].lon; // Obtain longitude of specified city
+        url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}`;
+        fetch (url)
+          .then ((response) => response.json())
+          .then ((data) => {
+            let currentWeather = document.getElementById("day-weather");
+            let skies = data.list[0].weather[0].main; // gets main weather type
+            let weatherIcon = weather_icon(skies);
+            currentWeather.children[3].children[0].textContent = weatherIcon;
+            currentWeather.children[3].children[1].textContent = `Currently ${Math.round(data.list[0].main.temp)-273}°C`; // Display current temperature
+            currentWeather.children[3].children[2].textContent = `Feels like ${Math.round(data.list[0].main.feels_like)-273}°C`; // Display feels like temperature
+            currentWeather.children[3].children[3].textContent = `Humidity: ${data.list[0].main.humidity}%`; // Set humidity
+            let direction = set_direction(data.list[0].wind.deg); // Get wind direction
+            currentWeather.children[3].children[4].textContent = `Wind Speed: ${data.list[0].wind.speed}km/h ${direction}`; // Set wind speed and direction
+          });
         fill_cards(city);
       } else {
         alert("City is invalid! Try again!");
+        document.getElementById("search-text").value = "";
     }
   });
 }
 
 function fill_cards (location) {
-  fetch (`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${api_key}`) // Default load to Toronto
+  fetch (`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${api_key}`) // Get lat and lon of target city
   .then((response) => response.json())
   .then((data) => {
-    let lat = data[0].lat; // Obtain latitude of Toronto
-    let lon = data[0].lon; // Obtain longitude of Toronto
+    let lat = data[0].lat; // Obtain latitude of city
+    let lon = data[0].lon; // Obtain longitude of city
     url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}`;
     fetch (url)
     .then ((response) => response.json())
@@ -64,12 +123,11 @@ function fill_cards (location) {
         let card = document.getElementById("day-"+i); // use i for local positioning
         let icon = weather_icon(data.list[index].weather[0].main); // use index for data array
         document.getElementById(`icon-${i}`).innerHTML = icon;
-        console.log(data.list[index].weather[0].main)
-        card.children[0].textContent = `Currently ${Math.round(data.list[index].main.temp)-273}°C`; // Display current temperature
+        card.children[0].textContent = `High ${Math.round(data.list[index].main.temp)-273}°C`; // Display current temperature
         card.children[1].textContent = `Feels like ${Math.round(data.list[index].main.feels_like)-273}°C`; // Display feels like temperature
         card.children[2].textContent = `Humidity: ${data.list[index].main.humidity}%`; // Set humidity
         let direction = set_direction(data.list[index].wind.deg); // Get wind direction
-        card.children[3].textContent = `Wind Speeds: ${data.list[index].wind.speed}km/h ${direction}`; // Set wind information
+        card.children[3].textContent = `Wind Speed: ${data.list[index].wind.speed}km/h ${direction}`; // Set wind information
       }
     });
   }
